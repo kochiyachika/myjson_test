61d971602f9ae47a9a1c60f77ec2ce6f7eee74da
Kochiya Chika <43286032+kochiyachika@users.noreply.github.com>
2023-10-12 13:02:32 +0800

    Update termux_install_script.sh

--- termux_install_script.sh
+++ termux_install_script.sh
@@ -7,19 +7,9 @@ unzip elichika && \
 # clean up the zip
 rm elichika.zip && \
 # rename to elichika
-mv -f elichika-master elichika && \
-cd elichika && \
-# download the patched database
-curl -L https://codeberg.org/arina999999997/nozomi_static_db/archive/master.zip -o static.zip && \
-# unzip and put the data base in correct place
-unzip static.zip && \
-mv nozomi_static_db static && \
-# remove the zip
-rm static.zip && \
+mv -f elichika-arina elichika && \
 # download the config file
 curl https://codeberg.org/arina999999997/nozomi/raw/branch/arina/catfolk_cdn.json -o config.json && \
-# download the server binary
-curl -L https://codeberg.org/arina999999997/nozomi/releases/download/0.0.1/elichika_android_arm64_8080_arina -o elichika && \
 # download the update script
 curl -L https://codeberg.org/arina999999997/nozomi/raw/branch/arina/termux_update_script.sh -o update.sh && \
 # set the permission
